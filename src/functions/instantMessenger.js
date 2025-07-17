ed643b385cc97c82f3998b677a56541adee2d654
fielr <61761822+fielr@users.noreply.github.com>
2025-07-17 20:37:02 +0900

    add composition listener to IM message input

--- src/functions/instantMessenger.js
+++ src/functions/instantMessenger.js
@@ -317,8 +317,15 @@ export default function instantMessenger() {
     }
   }
 
+  let isCompositing = false;
+  messageInput.addEventListener("compositionstart", () => {
+    isCompositing = true;
+  });
+  messageInput.addEventListener("compositionend", () => {
+    isCompositing = false;
+  });
   messageInput.addEventListener("keydown", (e) => {
-    if (e.key === "Enter" && !e.shiftKey) {
+    if (e.key === "Enter" && !e.shiftKey && !isCompositing) {
       e.preventDefault();
       if (BCX?.getRuleState("speech_restrict_beep_send")?.isEnforced && !fbcSettings.allowIMBypassBCX) {
         fbcNotify(displayText("Sending beeps is currently restricted by BCX rules"));
